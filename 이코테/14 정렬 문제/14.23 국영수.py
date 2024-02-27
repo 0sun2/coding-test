d4d3bdce45fbd17ed8835facf3f6969f4e91f4c8
0sun <dladudtjs9@gmail.com>
2024-02-27 18:01:07 +0900

    coding test

--- 이코테/14 정렬 문제/14.23 국영수.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-student_list = []
-for i in range(n):
-    student_list.append(input().split())
-student_list.sort(key= lambda x : (-int(x[1]),int(x[2]),-int(x[3]),x[0]))
-for student in student_list:
-    print(student[0])
