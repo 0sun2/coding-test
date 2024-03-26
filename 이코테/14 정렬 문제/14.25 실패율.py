32ba3f61a14ff23d54bf016ab4312de23ea39085
0sun <dladudtjs9@gmail.com>
2024-03-26 15:13:24 +0900

    coding_test

--- 이코테/14 정렬 문제/14.25 실패율.py
+++ 이코테/14 정렬 문제/14.25 실패율.py
@@ -1,27 +1,25 @@
 def solution(N, stages):
-    result_list = []
     answer = []
-    for i in range(N):
-        total = 0
-        count = 0
-        result = 0
-        for j in stages:
-            if j > i:
-                total += 1
-            elif j == i+1:
-                count += 1
-                total += 1
-            else:
-                continue
-        if not count == 0:
-          result = count / total
-        result_list.append((i+1,result))
-    result_list.sort(key = lambda x: - x[1])
-    for item in result_list:
-        answer.append(item[0])
+    length = len(stages)
 
+    # 스테이지 번호를 1부터 N까지 증가시키며
+    for i in range(1, N + 1):
+        # 해당 스테이지에 머물러 있는 사람의 수 계산
+        count = stages.count(i)
+        
+        # 실패율 계산
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+        
+        # 리스트에 (스테이지 번호, 실패율) 원소 삽입
+        answer.append((i, fail))
+        length -= count
+
+    # 실패율을 기준으로 각 스테이지를 내림차순 정렬
+    answer = sorted(answer, key=lambda t: t[1], reverse=True)
+    
+    # 정렬된 스테이지 번호 반환
+    answer = [i[0] for i in answer]
     return answer
-
-N = int(input())
-stages = list(map(int, input().split()))
-solution(N,stages)
